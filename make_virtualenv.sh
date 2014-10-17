994863303573f3e82e2748e04659480f72ebc220
Dmitry Otroshchenko <dmitry.otroshchenko@gmail.com>
2014-10-17 12:42:02 +0200

    Added a script to create virtualenvs.

---
+++ make_virtualenv.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# Create Python virtualenv
+# usage: make_virtualenv.sh <version> <virtualenv_name>
+
+set -e -x
+
+VERSION=$1
+VIRTUALENV_NAME=$2
+
+
+# Create virtualenvs root directory
+
+if [ "$WORKON_HOME" == "" ]
+then
+    WORKON_HOME=~/.virtualenvs
+fi
+
+mkdir -p $WORKON_HOME
+
+
+# Create new virtualenv with specified name and Python version
+
+[ -d "/System" ] && PREFIX="/System" || PREFIX="/opt"
+PYTHON="$PREFIX/python-$VERSION/bin/python${VERSION::3}"
+VIRTUALENV="$PREFIX/python-$VERSION/bin/virtualenv"
+
+$VIRTUALENV --python=$PYTHON --no-site-packages $WORKON_HOME/$VIRTUALENV_NAME
+
+
+# Install virtualenvwrapper for this virtualenv so that it doesn't break using tmux
+
+source $WORKON_HOME/$VIRTUALENV_NAME/bin/activate
+pip install virtualenvwrapper
+deactivate
+
