3b96ec2b16fc9ac68c0c9c688fe69cddc3b1e565
Dmitry Otroshchenko <d.otroshchenko@criteo.com>
2019-01-04 17:57:03 +0100

    Improve up installer.

--- install/up.fish
+++ install/up.fish
@@ -3,10 +3,12 @@
 type -q up
 if [ $status != 0 ]
     echo "Installing up.fish ..."
+    set HOME (echo ~)
+    set DOWNLOAD_PATH (mktemp --tmpdir=$HOME)
     set GITHUB_REPO 'https://raw.githubusercontent.com/shannonmoeller/up/master/up.fish'
-    set DOWNLOAD_PATH (mktemp)
     curl -o $DOWNLOAD_PATH $GITHUB_REPO
     type -q up ; or source $DOWNLOAD_PATH
+    rm -f $DOWNLOAD_PATH
 else
     echo "up.fish already installed."
 end
