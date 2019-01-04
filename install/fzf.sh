ecddc3db52b2f6cde1dc4930012e46b0bbf75b2d
Dmitry Otroshchenko <d.otroshchenko@criteo.com>
2019-01-04 18:23:37 +0100

    Fix fzf installer.

--- install/fzf.sh
+++ install/fzf.sh
@@ -7,7 +7,7 @@ if [ $? -ne 0 ]; then
     cd $LOCAL_INSTALL_ROOT
     GITHUB_REPO='https://github.com/junegunn/fzf.git'
     git clone --depth 1 $GITHUB_REPO $LOCAL_INSTALL_ROOT/.fzf
-    $LOCAL_INSTALL_ROOT/.fzf/install
+    $LOCAL_INSTALL_ROOT/.fzf/install --key-bindings --completion --update-rc
 else
     echo "fzf $(fzf --version) already installed."
 fi
