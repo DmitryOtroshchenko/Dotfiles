94c02648c9d7252c436cc4acf589c5df37898e7e
Dmitry Otroshchenko <d.otroshchenko@criteo.com>
2019-08-09 12:31:33 +0200

    Experimental fzf / ranger stuff.

--- config/fish/source/fzf_extras.fish
+++ config/fish/source/fzf_extras.fish
@@ -1,4 +1,17 @@
 
+function ask_confirmation -a prompt
+    while true
+        read -P $prompt' ' -l is_confirmed
+        switch (string lower $is_confirmed)
+            case 'yes' 'y'
+                return 0
+            case 'no' 'n'
+                return 1
+            case '*'
+        end
+    end
+end
+
 function fkill -a signal
     set -q signal[1]; or set signal '-9'
     set pids_to_kill (
@@ -8,7 +21,34 @@ function fkill -a signal
     )
     if set -q pids_to_kill[1]
         kill $signal $pids_to_kill
+        return 0
     else
         echo 'Kill aborted.'
+        return 1
+    end
+end
+
+function array_to_multiline -a arr
+    for el in $argv
+        echo $el
+    end
+end
+
+function n
+    set -l location_candidates (
+        j --complete $argv | awk -F'__' '{ OFS = ""; $1 = $2 = ""; print $0 }'
+    ); or return 1
+    switch (count $location_candidates)
+        case 0
+            echo 'No jump location candidates.'
+            return 1
+        case 1
+            cd $location_candidates[1]
+            return 0
+        case '*'
+            set -l jump_location \
+                (array_to_multiline $location_candidates | fzf --no-sort +m); \
+                and cd $jump_location
+            return $status
     end
 end
--- config/ranger/cmd/fzf.py
+++ config/ranger/cmd/fzf.py
@@ -5,52 +5,72 @@ import subprocess
 
 from ranger.api.commands import Command
 
+import iterfzf
+
+
+def run_and_merge_stdout(command):
+    '''From http://blog.kagesenshi.org/2008/02/teeing-python-subprocesspopen-output.html
+    https://www.saltycrane.com/blog/2009/10/how-capture-stdout-in-real-time-python/
+    '''
+    p = subprocess.Popen(
+        command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    line = '<non-empty>'
+    while line or p.poll() is None:
+        line = p.stdout.readline()
+        yield line.decode('utf8').strip()
+
 
 class fzf_select(Command):
-    """:fzf_select [-a | -d | -f]
+    ''':fzf_select [-a | -d | -f]
 
     Find a file using fzf.
-    """
+    '''
 
     FLAGS = {'-a', '-d', '-f'}
 
     def execute(self):
-        if len(self.args) == 1:
+        import sys
+        self.fm.notify(sys.executable, bad=False)
+        return
+        assert 2 <= len(self.args) <= 3, 'Invalid fzf_select call.'
+        if len(self.args) == 2:
+            root = self.args[1]
             mode = 'a'
-        elif len(self.args) == 2:
-            assert self.args[1] in self.FLAGS, 'Invalid fzf_select flag.'
-            mode = self.args[1][1]
+        elif len(self.args) == 3:
+            root = self.args[1]
+            assert self.args[2] in self.FLAGS, 'Invalid fzf_select flag.'
+            mode = self.args[2]
         else:
-            assert len(self.args) <= 2, 'Invalid fzf_select call.'
+            assert False
 
-        command = ' | '.join([
-            self._format_find_call(mode),
-            "sed 1d",
-            "cut -b3-",
-            "fzf +m",
-        ])
-        fzf = self.fm.execute_command(
-            command, universal_newlines=True, stdout=subprocess.PIPE)
-        stdout, stderr = fzf.communicate()
-        if fzf.returncode == 0:
-            fzf_file = os.path.abspath(stdout.rstrip('\n'))
+        chosen = self._fuzzy_choose_one(root, mode)
+        if chosen:
+            fzf_file = os.path.abspath(chosen)
             if os.path.isdir(fzf_file):
                 self.fm.cd(fzf_file)
             else:
                 self.fm.select_file(fzf_file)
         else:
-            self.fm.notify("FZF call failed", bad=True)
+            self.fm.notify('FZF call failed', bad=True)
+
+    def _fuzzy_choose_one(self, root, mode):
+        # self.fm.notify(f'{root} : {mode}', bad=False)
+        res = iterfzf.iterfzf(
+            run_and_merge_stdout(self._format_find_call(root, mode))
+        )
+        # self.fm.notify(f'Selected = {res}', bad=False)
+        return res
 
     @staticmethod
-    def _format_find_call(mode):
+    def _format_find_call(root, mode):
         type_selector_from_mode = {
-            'a': '',
-            'd': '-type d',
-            'f': '-not \( -type d \)',
+            '-a': '',
+            '-d': '-type d',
+            '-f': '-not \( -type d \)',
         }
         return (
-            "find -L . "
+            "find -L {} "
             "\( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) "
             "-prune -o {} -print "
             "2> /dev/null"
-        ).format(type_selector_from_mode[mode])
+        ).format(root, type_selector_from_mode[mode])
