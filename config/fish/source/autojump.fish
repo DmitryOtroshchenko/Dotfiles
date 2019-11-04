0314c003730054a1143b0b392c424a7e60fde238
Dmitry Otroshchenko <d.otroshchenko@criteo.com>
2019-11-04 16:55:00 +0100

    Use global autojump.

--- config/fish/source/autojump.fish
+++ config/fish/source/autojump.fish
@@ -1,4 +1,5 @@
-set autojump_setup_fish (echo ~)'/local/share/autojump/autojump.fish'
+# set autojump_setup_fish (echo ~)'/local/share/autojump/autojump.fish'
+set autojump_setup_fish '/usr/local/share/autojump/autojump.fish'
 if test -f $autojump_setup_fish
     source $autojump_setup_fish
 end
