17f7db2676d094fdc0c166d7647c1c5a8a683203
Dmitry Otroshchenko <dmitry.otroshchenko@airbnb.com>
2019-11-04 17:56:15 +0100

    Merge macos settings files.

--- install/setup_macos_defaults.sh
+++ install/setup_macos_defaults.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+# Force R to use UTF-8.
 defaults write org.R-project.R force.LANG en_US.UTF-8
+
+# Setup key repeat.
+defaults write -g InitialKeyRepeat -int 20
+defaults write -g KeyRepeat -int 2
--- install/setup_macos_key_repeat.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-defaults write -g InitialKeyRepeat -int 20
-defaults write -g KeyRepeat -int 2
