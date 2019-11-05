710e45f0bf986a14bf8cf2d83506f6e4155ed0b4
Dmitry Otroshchenko <dmitry.otroshchenko@airbnb.com>
2019-11-05 17:01:24 +0100

    Git VSCode settings.

--- install/bootstrap-mac.sh
+++ install/bootstrap-mac.sh
@@ -20,6 +20,13 @@ cd ~/Dotfiles
 ./install/setup_macos_defaults.sh
 confirm 'Install kbd layouts?' && ./install/install_layouts.sh
 
+# Dotfiles + dotbot. Install before casks so that all configs were already in place.
+which dotbot > /dev/null || { confirm 'Install dotbot?' && \
+    sudo /usr/bin/easy_install pip && sudo /usr/local/bin/pip install dotbot; }
+
+confirm 'Link common dotfiles?' && dotbot -c dotbot.yaml
+confirm 'Link Mac-specific dotfiles?' && dotbot -c dotbot-macos.yaml
+
 # Homebrew
 which brew > /dev/null || { confirm 'Install homebrew?' && \
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; }
@@ -42,15 +49,8 @@ install_formulae() {
 }
 confirm 'Install formulae?' && install_formulae
 
-# Dotfiles + dotbot
-which dotbot > /dev/null || { confirm 'Install dotbot?' && \
-    sudo /usr/bin/easy_install pip && sudo /usr/local/bin/pip install dotbot; }
-
-confirm 'Link common dotfiles?' && dotbot -c dotbot.yaml
-confirm 'Link Mac-specific dotfiles?' && dotbot -c dotbot-macos.yaml
-
 # Download creds and access tokens from lastpass.
-confirm 'Bootstrap secrets from lastpass?' && { 
+confirm 'Bootstrap secrets from lastpass?' && {
     lpass login dmitry.otroshchenko@gmail.com && lpass show -F dotfiles-private --notes > ~/Dotfiles/config/fish/source/00.credentials.fish; }
 
 # Misc
