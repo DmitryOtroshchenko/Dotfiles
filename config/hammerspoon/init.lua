688d03a840289fc1f28cd94c38bc6326ea90d102
Dmitry Otroshchenko <dmitry.otroshchenko@airbnb.com>
2019-11-15 18:33:25 +0100

    Move sound tweaks to a separate file.

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -1,4 +1,5 @@
-local wm = require('modules.window_management')
+require('modules.window_management')
+require('modules.sound')
 
 -- Reload hs.
 hs.hotkey.bind(
@@ -9,20 +10,3 @@ hs.hotkey.bind(
 -- Hotkey kitty.
 local app_man = require('modules.appman')
 hs.hotkey.bind({"cmd"}, "escape", app_man:toggle("kitty"))
-
--- Stfu when locked.
-hasBeenMutedBeforeLock = false
-
-function sleepWatch(eventType)
-  -- Set volume to 0 when locked.
-  if (eventType == hs.caffeinate.watcher.screensDidLock) then
-    hasBeenMutedBeforeLock = hs.audiodevice.defaultOutputDevice():muted()
-    hs.audiodevice.defaultOutputDevice():setMuted(true)
-    hs.spotify.pause()
-  elseif (not hasBeenMutedBeforeLock and eventType == hs.caffeinate.watcher.screensDidUnlock) then
-    hasBeenMutedBeforeLock = false
-    hs.audiodevice.defaultOutputDevice():setMuted(false)
-  end
-end
-
-hs.caffeinate.watcher.new(sleepWatch):start()
---
+++ config/hammerspoon/modules/sound.lua
@@ -0,0 +1,16 @@
+-- Stfu when locked.
+hasBeenMutedBeforeLock = false
+
+function sleepWatch(eventType)
+  -- Set volume to 0 when locked.
+  if (eventType == hs.caffeinate.watcher.screensDidLock) then
+    hasBeenMutedBeforeLock = hs.audiodevice.defaultOutputDevice():muted()
+    hs.audiodevice.defaultOutputDevice():setMuted(true)
+    hs.spotify.pause()
+  elseif (not hasBeenMutedBeforeLock and eventType == hs.caffeinate.watcher.screensDidUnlock) then
+    hasBeenMutedBeforeLock = false
+    hs.audiodevice.defaultOutputDevice():setMuted(false)
+  end
+end
+
+hs.caffeinate.watcher.new(sleepWatch):start()
