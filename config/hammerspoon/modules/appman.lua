9dc626df3e3458ca77b746515e7bc7525bb3a354
Dmitry Otroshchenko <dmitry.otroshchenko@airbnb.com>
2019-11-22 15:06:17 +0100

    Reformat hs appman.

--- config/hammerspoon/modules/appman.lua
+++ config/hammerspoon/modules/appman.lua
@@ -4,34 +4,34 @@ local obj = {}
 local app_mode = hs.hotkey.modal.new()
 
 function obj:toggle(name, secondName, isMoveMouse)
-    secondName = secondName or "--no-app--"
-    isMoveMouse = isMoveMouse or false
+  secondName = secondName or "--no-app--"
+  isMoveMouse = isMoveMouse or false
 
-    return function()
-        local activated = hs.application.frontmostApplication()
-        local path = string.lower(activated:path())
+  return function()
+    local activated = hs.application.frontmostApplication()
+    local path = string.lower(activated:path())
 
-        if (string.match(path, string.lower(name) .. "%.app$")
-                or string.match(path, string.lower(secondName) .. "%.app$")) then
-            return activated:hide()
-        end
-
-        if not hs.application.launchOrFocus(name) then
-            hs.application.launchOrFocus(secondName)
-        end
-
-        if isMoveMouse then
-            local screen = hs.window.focusedWindow():frame()
-            local pt = hs.geometry.rectMidPoint(screen)
-            hs.mouse.setAbsolutePosition(pt)
-        end
+    if (string.match(path, string.lower(name) .. "%.app$")
+        or string.match(path, string.lower(secondName) .. "%.app$")) then
+      return activated:hide()
     end
+
+    if not hs.application.launchOrFocus(name) then
+      hs.application.launchOrFocus(secondName)
+    end
+
+    if isMoveMouse then
+      local screen = hs.window.focusedWindow():frame()
+      local pt = hs.geometry.rectMidPoint(screen)
+      hs.mouse.setAbsolutePosition(pt)
+    end
+  end
 end
 
 local function focusScreen(screen)
   local windows = hs.fnutils.filter(
-      hs.window.orderedWindows(),
-      hs.fnutils.partial(isInScreen, screen))
+    hs.window.orderedWindows(),
+    hs.fnutils.partial(isInScreen, screen))
   local windowToFocus = #windows > 0 and windows[1] or hs.window.desktop()
   windowToFocus:focus()
 
@@ -40,16 +40,8 @@ local function focusScreen(screen)
   hs.mouse.setAbsolutePosition(center)
 end
 
-function obj.focusPreviousScreen()
-    focusScreen(hs.window.focusedWindow():screen():previous())
-end
-
-function obj.focusNextScreen()
-    focusScreen(hs.window.focusedWindow():screen():next())
-end
-
 function obj:init(mod, key)
-    return self
+  return self
 end
 
 return obj
