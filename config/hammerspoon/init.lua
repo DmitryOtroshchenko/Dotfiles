e29414514ca9b06a34532d228af90a430fcd2d52
Dmitry Otroshchenko <dmitry.otroshchenko@airbnb.com>
2019-11-22 14:38:40 +0100

    Move hammerspoon menu to a separate file.

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -1,5 +1,9 @@
-require("modules.window_management")
+local app_man = require("modules.appman")
+
+require("modules.menu")
 require("modules.sound")
+require("modules.window_management")
+
 
 hs.application.enableSpotlightForNameSearches(true)
 
@@ -12,37 +16,6 @@ hs.hotkey.bind(
 )
 
 -- Hotkey kitty.
-local app_man = require("modules.appman")
 hs.hotkey.bind({"cmd"}, "escape", app_man:toggle("kitty"))
 
-
 local log = hs.logger.new("root", "debug")
-
-local menu_bar = hs.menubar.new()
-local PlaybackStateIcons = {
-  [hs.spotify.state_playing]=" ",
-  [hs.spotify.state_paused]="•",
-  [hs.spotify.state_stopped]="■"
-}
--- Important: if not assigned to a variable, timers get garbage collected.
-SpotifyStatusTimer = hs.timer.doEvery(
-  0.5,
-  function()
-    local menuBarText = ""
-    -- Update spotify status.
-    if (hs.spotify.isRunning()) then
-      local unknown = "UNKNOWN"
-      local playbackIcon = PlaybackStateIcons[hs.spotify.getPlaybackState()] or " "
-      local artist = hs.spotify.getCurrentArtist() or unknown
-      local track = hs.spotify.getCurrentTrack() or unknown
-      menuBarText = playbackIcon .. " " .. artist .. ' - ' .. track
-    end
-    -- Update CPU usage.
-    local cpuLoad = math.ceil(hs.host.cpuUsage()["overall"]["active"])
-    if (cpuLoad >= 0) then
-      menuBarText = menuBarText .. "  " .. cpuLoad .. "%"
-    end
-    -- Update
-    menu_bar:setTitle(menuBarText)
-  end
-)
---
+++ config/hammerspoon/modules/menu.lua
@@ -0,0 +1,75 @@
+
+local log = hs.logger.new("menu", "debug")
+
+local PlaybackStateIcons = {
+  [hs.spotify.state_playing]=" ",
+  [hs.spotify.state_paused]="•",
+  [hs.spotify.state_stopped]="■"
+}
+
+function renderSpotifyStatus()
+  if (hs.spotify.isRunning()) then
+    local unknown = "UNKNOWN"
+    local playbackIcon = PlaybackStateIcons[hs.spotify.getPlaybackState()] or " "
+    local artist = hs.spotify.getCurrentArtist() or unknown
+    local track = hs.spotify.getCurrentTrack() or unknown
+    return hs.styledtext.new(
+      playbackIcon .. " " .. artist .. ' - ' .. track,
+      {font={name="Source Code Pro"}}
+    )
+  end
+  return ""
+end
+
+function renderCpuAlert(loadThreshold)
+  loadThreshold = loadThreshold or 75
+  assert(loadThreshold >= 0 and loadThreshold <= 100)
+  local cpuLoad = math.ceil(hs.host.cpuUsage()["overall"]["active"])
+  if (cpuLoad >= 75) then
+    local cpuLoadRepr = tostring(cpuLoad)
+    if (cpuLoadRepr:len() < 2) then
+      cpuLoadRepr = " " .. cpuLoadRepr
+    end
+    return hs.styledtext.new(
+      "CPU = " .. cpuLoadRepr .. "%",
+      {
+        font={name="Source Code Pro"},
+        color=hs.drawing.color.hammerspoon.osx_red
+      }
+    )
+  end
+  return ""
+end
+
+function renderMenuWidgets(widgets)
+  local menuBarText = hs.styledtext.new("")
+  for _, widgetFunc in pairs(widgets) do
+    local widgetText = widgetFunc() or ""
+    if (type(widgetText) == "string") then
+      widgetText = hs.styledtext.new(widgetText)
+    end
+    menuBarText = menuBarText .. widgetText
+  end
+  return menuBarText
+end
+
+function spacer(nSpaces)
+  nSpaces = nSpaces or 2
+  assert(not nSpaces or nSpaces >= 0)
+  return (" "):rep(nSpaces)
+end
+
+local MenuBar = hs.menubar.new()
+local MenuBarWidgets = {
+  renderCpuAlert,
+  spacer,
+  renderSpotifyStatus
+}
+-- Important: if not assigned to a variable, timers get garbage collected.
+MenuRefreshTimer = hs.timer.doEvery(
+  0.5,
+  function()
+    local rendered = renderMenuWidgets(MenuBarWidgets)
+    MenuBar:setTitle(rendered)
+  end
+)
