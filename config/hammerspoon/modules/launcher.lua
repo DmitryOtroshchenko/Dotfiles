e236a0c25300943fd629175749bacd679ff82bdc
Dmitry Otroshchenko <dmitry.otroshchenko@airbnb.com>
2019-12-12 14:03:33 +0100

    Improve app launcher mode.

--- config/hammerspoon/modules/launcher.lua
+++ config/hammerspoon/modules/launcher.lua
@@ -7,35 +7,58 @@
 -- https://github.com/Hammerspoon/hammerspoon/issues/848
 -- https://github.com/cmsj/hammerspoon-config/blob/master/init.lua
 
-function launchOrFocusCall(app)
-  return function ()
-    hs.application.launchOrFocus(app)
+function lf(app)
+  hs.application.launchOrFocus(app)
+end
+
+function lfAndMove(app, x, y, w, h)
+  local hasBeenRunning = hs.application.find(app) ~= nil
+  lf(app)
+  if (hasBeenRunning) then
+    hs.window.focusedWindow():moveToUnit({x, y, w, h})
   end
 end
 
+function lfAndMaximize(app)
+  lfAndMove(app, 0, 0, 1, 1)
+end
+
+PreviousAppActivated = nil
+
+LastAppWatcher = hs.application.watcher.new(
+  function(appName, eventType, app)
+    if (PreviousAppActivated ~= appName) then
+      PreviousAppActivated = app:path()
+    end
+  end
+):start()
+
 AppLauncherApps = {
-  ["n"] = launchOrFocusCall("Vivaldi"),
-  ["e"] = launchOrFocusCall("Visual Studio Code"),
-  ["space"] = launchOrFocusCall("kitty"),
-  ["m"] = launchOrFocusCall("Slack"),
+  ["n"] = function() lfAndMaximize("Vivaldi") end,
+  ["e"] = function() lfAndMaximize("Visual Studio Code") end,
+  ["space"] = function() lfAndMaximize("kitty") end,
+  ["m"] = function() lfAndMaximize("Slack") end,
+  ["f20"] = function() lf(PreviousAppActivated) end,
   ["0"] = hs.toggleConsole
 }
 
-AppLauncherMode = hs.hotkey.modal.new({"cmd"}, "escape", nil)
+IsAppLauncherMode = false
+AppLauncherMode = hs.hotkey.modal.new({}, "f20", nil)
+AppLauncherMode.entered = function () IsAppLauncherMode = true end
+AppLauncherMode.exited = function () IsAppLauncherMode = false end
 
 AppLauncherModeKeyEvent = hs.eventtap.new(
   {hs.eventtap.event.types.keyDown},
   function (event)
+    if (not IsAppLauncherMode) then return end
+
     local keyPressed = hs.keycodes.map[event:getKeyCode()]
     local action = AppLauncherApps[keyPressed]
     if (action ~= nil) then action() end
     AppLauncherMode:exit()
-    AppLauncherModeKeyEvent:stop()
     return true
   end
-)
-
-AppLauncherMode.entered = function () AppLauncherModeKeyEvent:start() end
+):start()
 
 -- Seal
 hs.loadSpoon("Seal")
