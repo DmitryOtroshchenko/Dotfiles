03f9c3b29fc4c29014118b29ddefc84922ceaaf0
Dmitry Otroshchenko <dmitry.otroshchenko@airbnb.com>
2020-03-02 22:14:05 +0100

    HS: functional rewrite of fuzzy chooser.

--- config/hammerspoon/modules/fuzzy_chooser.lua
+++ config/hammerspoon/modules/fuzzy_chooser.lua
@@ -1,19 +1,21 @@
 require("common")
 
+local std = require("std")
+local func = require("std.functional")
 local string = require("std.string")
 local table = require("std.table")
 local io = require("std.io")
 
 FuzzyChooser = {}
 FuzzyChooser.__index = FuzzyChooser
+FuzzyChooser.delimiter = ":"
 
 function FuzzyChooser:create(items)
   local obj = {}
   setmetatable(obj, FuzzyChooser)
 
   obj.items = items
-  obj.appChooser = hs.chooser
-    .new(withself(obj, obj._queryAccepted))
+  obj.appChooser = hs.chooser.new(withself(obj, obj._queryAccepted))
     :choices(FuzzyChooser._appTableToChoices(items))
     :queryChangedCallback(withself(obj, obj._queryChanged))
 
@@ -36,40 +38,38 @@ function FuzzyChooser:_queryAccepted(choice)
 end
 
 function FuzzyChooser._appTableToChoices(apps)
-  local choices = {}
-  for _, app in pairs(apps) do
-    choices[#choices+1] = {
-      ["text"] = app["text"],
-      ["subText"] = "[" .. app["hotkey"] .. "]",
-      ["hotkey"] = app["hotkey"]
-    }
-  end
-  return choices
+  return func.map(
+    function (app)
+      return { text = app["text"], subText = "[" .. app["hotkey"] .. "]" }
+    end,
+    std.ielems,
+    apps
+  )
 end
 
 function FuzzyChooser._runSk(items, query)
-  local delimiter = ":"
-  local itemsStr = ""
-  for ix, item in pairs(items) do
-    itemsStr = itemsStr .. "\n" .. tostring(ix) .. delimiter .. item["text"]
-  end
-  itemsStr = string.trim(itemsStr)
+  local itemsStr = func.foldl(
+    function (l, r) return l .. "\n" .. r end,
+    func.map(
+      function (ix, item) return ix .. FuzzyChooser.delimiter .. item["text"] end,
+      std.npairs,
+      items
+    )
+  )
 
-  local skCommand = "printf \'"..itemsStr.."\' | /usr/local/bin/sk -d\'"..delimiter.."\' --nth=2 -f \'"..string.escape_shell(query).."\'"
-  local skOutput = string.split(io.shell(skCommand), '\n')
-  local filteredItems = {}
-  for _, skLine in ipairs(skOutput) do
-    if #skLine > 0 then
-      filteredItems[#filteredItems+1] = FuzzyChooser._skToChoice(skLine)
-    end
+  local skCommand = "printf \'"..itemsStr.."\' | /usr/local/bin/sk -d\'"..FuzzyChooser.delimiter.."\' --nth=2 -f \'"..string.escape_shell(query).."\'"
+  local skOutput = string.trim(io.shell(skCommand))
+  if (#skOutput > 0) then
+    local skLines = string.split(skOutput, "\n")
+    local filteredItems = func.map(FuzzyChooser._skToChoice, std.ielems, skLines)
+    return table.sort(filteredItems, function (a, b) return a.score > b.score end)
   end
-  table.sort(filteredItems, function (a, b) return a.score > b.score end)
-  return filteredItems
+  return {}
 end
 
 function FuzzyChooser._skToChoice(sk)
   local scoreAndChoice = string.split(sk, "\t")
-  local choiceIxAndText = string.split(scoreAndChoice[2], ":")
+  local choiceIxAndText = string.split(scoreAndChoice[2], FuzzyChooser.delimiter)
   return {
     score = tonumber(scoreAndChoice[1]),
     text = choiceIxAndText[2],
