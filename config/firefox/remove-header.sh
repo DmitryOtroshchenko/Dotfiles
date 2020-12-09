9942442df8cf76bac536b91be13b3b3bfb3882ff
Dmitry Otroshchenko <dmitry.otroshchenko@airbnb.com>
2020-12-09 12:58:05 +0100

    wip

---
+++ config/firefox/remove-header.sh
@@ -0,0 +1,7 @@
+
+# See https://hackernoon.com/firefox-hide-tabs-6e1f6cf63d95
+
+# Check '/Users/dmitry_otroshchenko/Library/Application Support/Firefox/Profiles' folder.
+# It should contain folders with names like '193vo7o4.default-release/'.
+# Each of the folders correspond to user's profile (?).
+# Copy chrome/ with all its contents into your profile folder.
